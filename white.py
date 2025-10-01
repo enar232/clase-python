5eebc0bfb4147e83d8a217a95ed9d29f2e5c6f39
emalvarez.es <emalvarez.es@tecnologicoargos.edu.ec>
2025-10-01 00:07:38 +0000

    clasewhite

---
+++ white.py
@@ -0,0 +1,20 @@
+contador  = 0
+
+while contador < 5:
+     print("contador:", contador)
+     contador += 1 #contador = contador + 1 
+
+
+isMayorEdad = True
+edad = 25 
+
+while isMayorEdad:
+    if edad < 18: 
+        print("Es menor de edad")
+        isMayorEdad = False 
+
+    edad -= 1 
+    print("Edad:" , edad)
+
+print("Fin del ciclo")
+
